3bb457465ac96a119a11f1ba845c0b09b69031fc
Yannick Stoffers <yannick@svcover.nl>
2019-01-17 15:38:57 +0100

    Added a page for a background colour picker for the logo.

---
+++ kleurtjes/index.php
@@ -0,0 +1,25 @@
+<?php
+set_include_path('../');
+require_once '../include/init.php';
+
+/** Renders and processes CRUD operations for the Signup Model */
+class LogoColourView extends TemplateView
+{
+    /** 
+     * Run the page, but only for logged in committee members. 
+     * Non-admins are only allowed to see a list of their redirects
+     */
+    public function run_page() {
+        if (!cover_session_logged_in() && !DEBUG)
+            throw new HttpException(401, 'Unauthorized', sprintf('<a href="%s" class="btn btn-primary">Login and get started!</a>', cover_login_url()));
+        else if (!cover_session_in_committee(ADMIN_COMMITTEE) && !DEBUG)
+            throw new HttpException(403, 'You need to be a member of the ExCee to see this page!');
+        else
+            return parent::run_page();
+    }
+}
+
+// Create and run home view
+$view = new LogoColourView('kleurtjes/index');
+
+$view->run();
