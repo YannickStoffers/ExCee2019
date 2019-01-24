cec3a2899221b8aa60b0884fe569b18019c0feec
Yannick Stoffers <yannick@svcover.nl>
2019-01-24 16:48:24 +0100

    Combined sql setup.

--- include/models/migrations/000_init.sql
+++ include/models/migrations/000_init.sql
@@ -27,3 +27,40 @@ CREATE TABLE `registrations` (
   PRIMARY KEY (`id`),
   UNIQUE (`email`)
 ) ENGINE = INNODB;
+
+-- 
+-- Table structure for table `programme`
+-- 
+
+DROP TABLE IF EXISTS `programme`;
+CREATE TABLE `programme` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `section` enum('pre-excursion','city-01','city-02','city-03','post-excursion') NOT NULL DEFAULT 'pre-excursion',
+  `date` date,
+  `time` time,
+  `title` varchar(255),
+  `description` varchar(4096),
+  `image_name` varchar(30),
+  `company_visit` tinyint(1) NOT NULL DEFAULT '0',
+  `company_id` int,
+  PRIMARY KEY (`id`)
+) ENGINE = INNODB;
+
+-- 
+-- Table structure for table `companies`
+-- 
+
+DROP TABLE IF EXISTS `companies`;
+CREATE TABLE `companies` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50),
+  `description` varchar(4096),
+  `home_page` varchar(255),
+  `logo_url` varchar(255),
+  `activity_date` date,
+  `activity_time` time,
+  `activity_description` varchar(4096),
+  PRIMARY KEY (`id`),
+  UNIQUE(`name`)
+) ENGINE = INNODB;
+
